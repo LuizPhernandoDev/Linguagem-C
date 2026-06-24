1153b74e7a3efbafa6cbff02f944809dd554ca0f
Luiz Phernando Neves Alves de Souza <luizphernando2905@gmail.com>
2026-06-24 16:46:34 -0300

    2° commit - printf normal

---
+++ imprimindo/normal/ola_mundo2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+	
+	printf("Ola ");
+	printf("mundo!\n");
+
+	return 0;
+}
---
+++ imprimindo/normal/ola_mundo3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+	
+	printf("O");
+	printf("l");
+	printf("a");
+	printf(" ");
+	printf("m");
+	printf("u");
+	printf("n");
+	printf("d");
+	printf("o");
+	printf("!");
+	printf("\n");
+
+	return 0;
+}
