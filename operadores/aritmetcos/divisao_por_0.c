9e7aa5f3d025ffdc15adb218801c82a2ea591822
Luiz Phernando Neves Alves de Souza <luizphernando2905@gmail.com>
2026-06-26 20:58:24 -0300

    21° commit - adicionando a excusão do 0 na divisao

---
+++ operadores/aritmetcos/divisao_por_0.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+	printf("%d\n", 1 / 0);    // Impossível dividir por zero
+	//printf("%d\n", 1 % 0);  // O operador módulo % também não aceita zero como operando
+	printf("Esta instrução nunca será executada - isso não será escrito na tela\n");
+
+/*
+gera warning (aviso na compilação)
+e erro em tempo de execução - programa termina e não executa
+as próximas instruções
+*/
+	
+	return 0;              
+}
