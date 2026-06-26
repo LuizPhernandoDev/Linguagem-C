c377616f103308c847ffacf5bd2e432bfca94719
Luiz Phernando Neves Alves de Souza <luizphernando2905@gmail.com>
2026-06-25 22:41:00 -0300

    15° commit - criação da tipo_sem_sinal

--- variaveis/tipo4_com_sinal.c
+++ variaveis/tipo4_com_sinal.c
@@ -15,10 +15,15 @@ int main(){
 	signed long Int3;
 	signed long long Int4;
 	
+	/*
+	é redundante colocar a variavel como signed,
+	pois normalmente já é o pardrão, exeto na do tipo char,
+	já que ela defini que ele será tratado como numeral ao invez de caracter 
+	*/  
 	printf("Tipos de variaveis | Especificadores | Tamanho (Bytes) |                  Intervalo\n");
 	printf("                   |   de formato    |                 |\n");
 	printf("-------------------|-----------------|-----------------|--------------------------------------------\n");
-	printf("   signed char     |     %%c ou %%s    |        %zu        |                 %d a %d  \n", sizeof(signed char), SCHAR_MIN, SCHAR_MAX);
+	printf("   signed char     |       %%hhd      |        %zu        |                 %d a %d  \n", sizeof(signed char), SCHAR_MIN, SCHAR_MAX);
 	printf("   signed short    |       %%hd       |        %zu        |               %d a %d         \n", sizeof(signed short), SHRT_MIN, SHRT_MAX);
 	printf("    signed int     |     %%d ou %%i    |        %zu        |          %d a %d         \n", sizeof(signed int), INT_MIN, INT_MAX);
 	printf("   signed long     |       %%ld       |        %zu        |          %ld a %ld         \n", sizeof(signed long), LONG_MIN, LONG_MAX);
---
+++ variaveis/tipo5_sem_sinal.c
@@ -0,0 +1,33 @@
+// diretiva para ignorar o limite de 8 Bytes para valores de ponto flutuantes pre definida no windows
+#define __USE_MINGW_ANSI_STDIO 1
+
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+#include <math.h>
+
+int main(){
+	
+	// valores inteiros
+	unsigned char Char;
+	unsigned short Int1;
+	unsigned int Int2;
+	unsigned long Int3;
+	unsigned long long Int4;
+	
+	/*
+	é redundante colocar a variavel como signed,
+	pois normalmente já é o pardrão, exeto na do tipo char,
+	já que ela defini que ele será tratado como numeral ao invez de caracter 
+	*/  
+	printf("Tipos de variaveis | Especificadores | Tamanho (Bytes) |         Intervalo\n");
+	printf("                   |   de formato    |                 |\n");
+	printf("-------------------|-----------------|-----------------|--------------------------\n");
+	printf("  unsigned char    |       %%hhu      |        %zu        | 0 a %u\n", sizeof(unsigned char), UCHAR_MAX);
+	printf("  unsigned short   |       %%hu       |        %zu        | 0 a %u\n", sizeof(unsigned short), USHRT_MAX);
+	printf("   unsigned int    |       %%u        |        %zu        | 0 a %u\n", sizeof(unsigned int), UINT_MAX);
+	printf("  unsigned long    |       %%lu       |        %zu        | 0 a %lu\n", sizeof(unsigned long), ULONG_MAX);
+	printf("unsigned long long |       %%llu      |        %zu        | 0 a %llu\n", sizeof(unsigned long long), ULLONG_MAX);
+	
+  return 0;
+}
