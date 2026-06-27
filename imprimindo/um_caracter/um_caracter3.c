70263a5f4b8cdc0a849a3ded8f6a91f880f95fa6
Luiz Phernando Neves Alves de Souza <luizphernando2905@gmail.com>
2026-06-27 20:01:19 -0300

    23° commit - correções de ortografia e nomenclatura

--- imprimindo/um_caracter/um_caracter3.c
+++ imprimindo/um_caracter/um_caracter3.c
@@ -2,9 +2,9 @@
 
 int main(){
 	
-	char O = 'O', l = 'l', a = 'a', Espaco = ' ', m = 'm', u = 'u', n = 'n', d = 'd', o = 'o', Interrogacao = '!', PulaLinha = '\n';
+	char O = 'O', l = 'l', a = 'a', Espaco = ' ', m = 'm', u = 'u', n = 'n', d = 'd', o = 'o', Exclamacao = '!', PulaLinha = '\n';
 	
-	printf("%c%c%c%c%c%c%c%c%c%c%c", O, l, a, Espaco, m, u, n, d, o, Interrogacao, PulaLinha);
+	printf("%c%c%c%c%c%c%c%c%c%c%c", O, l, a, Espaco, m, u, n, d, o, Exclamacao, PulaLinha);
 	
 	return 0;
 }
