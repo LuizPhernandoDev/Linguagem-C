9579eb4ef0df2990170b1756006e48d5e98ea616
Luiz Phernando Neves Alves de Souza <luizphernando2905@gmail.com>
2026-06-28 21:50:44 -0300

    34 commit - adicionando novo tipo de variavel boleana

--- variaveis/boleanas/tipo2.c
+++ variaveis/boleanas/tipo2.c
@@ -1,13 +1,17 @@
+
 /*
 por mais que esse tipo é nativa da linguagem,
 coloquei ele aqui para ficar mais compreensivo
 */
 #include <stdio.h>
 
+#define true 1
+#define false 0
+
 int main() {
     
-    _Bool Verdadeiro = 1; 
-    _Bool Falso = 0;
+    _Bool Verdadeiro = true; 
+    _Bool Falso = false;
     
     printf("Tipos de variaveis | Especificadores | Tamanho (Bytes) | Intervalo\n");
 	printf("                   |   de formato    |                 |\n");
